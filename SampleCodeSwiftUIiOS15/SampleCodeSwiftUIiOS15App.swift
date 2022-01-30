9d5b4a1d5c3ef69d21ffd04cbfeee62d713f36ea
Thavorn Kamolsin <ka.thavorn@gmail.com>
2022-01-30 16:08:24 +0700

    Init Project

---
+++ SampleCodeSwiftUIiOS15/SampleCodeSwiftUIiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCodeSwiftUIiOS15App.swift
+//  SampleCodeSwiftUIiOS15
+//
+//  Created by PrInCeInFiNiTy on 30/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SampleCodeSwiftUIiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
